168600c7671ebf3d9269e393be80df7b55ee9277
zfeller <zfeller@ucsc.edu>
2024-10-09 16:25:25 -0700

    install express and make basic app

---
+++ app.ts
@@ -0,0 +1,9 @@
+// app.ts
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
