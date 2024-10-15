5b842dbf16a673ad301ebbbfeb7c496494c30302
“Keaton <kshawhan@ucsc.edu>
2024-10-15 15:43:28 -0700

    built schema file - nico trying to setup xcode bs

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,8 +1,49 @@
+-- Users Table
+DROP TABLE IF EXISTS user CASCADE;
+CREATE TABLE user (
+    id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(),
+    data jsonb
+);
 
-DROP TABLE IF EXISTS fruit CASCADE;
+-- Posts Table
+DROP TABLE IF EXISTS post CASCADE;
+CREATE TABLE post (
+    id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID REFERENCES user(id) ON DELETE CASCADE,
+    data jsonb
+);
 
-CREATE TABLE fruit(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), data jsonb);
+-- Sponsors Table
+DROP TABLE IF EXISTS sponsor CASCADE;
+CREATE TABLE sponsor (
+    id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(),
+    active BOOLEAN,
+    data jsonb
+);
 
+-- Follow Table
+DROP TABLE IF EXISTS follow CASCADE;
+CREATE TABLE follow (
+    sender UUID REFERENCES user(id) ON DELETE CASCADE,
+    receiver UUID REFERENCES user(id) ON DELETE CASCADE,
+    PRIMARY KEY (sender, receiver),
+    CHECK (sender != receiver)
+);
 
-CREATE TABLE image(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), data jsonb);
+-- To Eat Table
+DROP TABLE IF EXISTS toEat CASCADE;
+CREATE TABLE toEat (
+    post_id UUID REFERENCES post(id) ON DELETE CASCADE,
+    user_id UUID REFERENCES user(id) ON DELETE CASCADE,
+    PRIMARY KEY (post_id, user_id)
+);
+
+-- Recommend Table
+DROP TABLE IF EXISTS review CASCADE;
+CREATE TABLE recommend (
+    user_id UUID REFERENCES user(id) ON DELETE CASCADE,
+    dish TEXT NOT NULL,
+    rating INT CHECK (rating >= 0 AND rating <= 5),
+    PRIMARY KEY (user_id, dish)
+);
 
