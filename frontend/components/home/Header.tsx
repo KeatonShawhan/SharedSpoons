40bbee3fb40ef63ddfccb95a08154c048345289b
jgurien03 <jgurien@ucsc.edu>
2024-10-30 04:55:37 -0700

    added main page comments, scrolling, headers, modularity, etc

---
+++ frontend/components/home/Header.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { Colors } from '@/constants/Colors';
+
+const ORANGE_COLOR = '#FF9F45';
+
+interface HeaderProps {
+  colorScheme: string;
+}
+
+export const Header: React.FC<HeaderProps> = ({ colorScheme }) => (
+  <View style={styles.headerContainer}>
+    <View style={styles.titleContainer}>
+      <Text style={[styles.title, { color: Colors[colorScheme].text }]}>
+        SharedSpoons
+      </Text>
+      <Ionicons 
+        name="restaurant" 
+        size={28} 
+        color={Colors[colorScheme].text} 
+        style={styles.icon}
+      />
+    </View>
+    <View style={styles.divider} />
+  </View>
+);
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    paddingTop: 50,
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    paddingBottom: 15,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: '800',
+  },
+  icon: {
+    marginLeft: 'auto',
+    paddingTop: 2
+  },
+  divider: {
+    height: 2,
+    backgroundColor: ORANGE_COLOR,
+    marginHorizontal: 20,
+  },
+});
--- frontend/components/postCard/postImage.tsx
+++ frontend/components/postCard/postImage.tsx
@@ -1,24 +1,27 @@
+// components/post/postImage.tsx
 import { PropsWithChildren } from 'react';
-import { Image, View} from 'react-native';
-
+import { Image, View } from 'react-native';
 import { ThemedView } from '@/components/ThemedView';
 import { addButton } from './addButton';
-import { postCaption } from './postCaption';
 
-export function postImage({image}: { image:string }) {
+interface postImageProps {
+  image: string;
+}
+
+export function postImage({ image }: postImageProps): React.JSX.Element {
   return (
-      <ThemedView >
-        <View>
-          <View style={{position: 'relative', width: '100%', height: 350, paddingHorizontal: 8, paddingVertical: 3}}>
-            <Image
-              style={{width: '100%', height: 350, borderRadius: 20}}
-              source={{
-                uri: image,
-              }}
-            />
-            {addButton()}
-          </View>
+    <ThemedView>
+      <View>
+        <View style={{position: 'relative', width: '100%', height: 350, paddingHorizontal: 8, paddingVertical: 3}}>
+          <Image
+            style={{width: '100%', height: 350, borderRadius: 20}}
+            source={{
+              uri: image,
+            }}
+          />
+          {addButton()}
         </View>
-      </ThemedView>
+      </View>
+    </ThemedView>
   );
 }
