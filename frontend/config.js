bd5bf07c40d4478b082d949ecb7d43b91af2ac0f
jgurien03 <jgurien@ucsc.edu>
2024-11-07 15:05:11 -0800

    adding

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,2 @@
-const API_URL = "http://localhost/api/v0/"; // Adjust for your deployed URL when necessary
+const API_URL = "http://localhost:3010/api/v0/"; // Adjust for your deployed URL when necessary
 export default API_URL;
